b941c7b0cfcf20c31b81cd9a0bf50ecba4b0c64b
Stanislav001 <bezqk14@abv.bg>
2021-12-31 18:14:44 +0200

    Update Counst

--- Services/Common/Const.cs
+++ Services/Common/Const.cs
@@ -2,6 +2,9 @@
 {
     public class Const
     {
+        public static string NullElementsMessage =
+           "This file contains no items!";
+
         public const int MAX_REGION_NAME_CHARS = 56;
         public const int MAX_COUNTRY_NAME_CHARS = 56;
     }
