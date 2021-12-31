bc2c8e260c0a45a6f9fb69e0024c1ead6ebefc78
Stanislav001 <bezqk14@abv.bg>
2021-12-31 16:51:11 +0200

    Remove password prop in User model

--- Models/User.cs
+++ Models/User.cs
@@ -5,7 +5,6 @@ namespace Models
     public class User : BaseModel
     {
         public string UserName { get; set; }
-        public string Password { get; set; }
         public string PasswordHash { get; set; }
     }
 }
