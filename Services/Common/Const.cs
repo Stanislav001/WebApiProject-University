9596e70d17252712ad403b62086c95ffa340177b
Stanislav001 <bezqk14@abv.bg>
2021-12-31 18:42:31 +0200

    Add new Exception

--- Services/Common/Const.cs
+++ Services/Common/Const.cs
@@ -5,6 +5,9 @@
         public static string NullElementsMessage =
            "This file contains no items!";
 
+        public static string WrongFormatMessage =
+           "This format is unreadable!";
+
         public const int MAX_REGION_NAME_CHARS = 56;
         public const int MAX_COUNTRY_NAME_CHARS = 56;
     }
